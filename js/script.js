74587d6c3dee249e54d40456a0872fd921c9a493
LeiLoliLu <leiregose@gmail.com>
2024-04-11 13:50:14 +0200

    Ayaya

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-var townsfolk = 
+const townsfolk = [ 
+    {"name":"Shyam", "greeting":"Hola, Soy Shyam"},  
+    {"name":"Bob", "greeting":"Hola, Soy Bob"},  
+    {"name":"Jai", "greeting":"Hola, Soy Jai"},
+    {"name":"Miriam", "greeting":"Hola, Soy Miriam"},
+    {"name":"Reuben", "greeting":"Hola, Soy Reuben"},  
+    {"name":"Macie", "greeting":"Hola, Soy Macie"},  
+    {"name":"Zackery", "greeting":"Hola, Soy Zackery"},
+    {"name":"Brielle", "greeting":"Hola, Soy Brielle"},
+    {"name":"Cale", "greeting":"Hola, Soy Cale"},  
+    {"name":"Angelique", "greeting":"Hola, Soy Angelique"}];
+var hasClient = false;
+var lastClient = 10;
 
 //declaración de variables
 var title = document.getElementById("title");
@@ -6,7 +18,6 @@ var texttitle = document.getElementById("texttitle");
 var box = document.getElementById("box");
 var textbox = document.getElementById("textbox");
 var screen = document.getElementById("screen");
-var hasClient = false;
 
 var bJardin = document.getElementById("bJardin");
 var bMostrador = document.getElementById("bMostrador");
@@ -22,20 +33,16 @@ const botonRec = bRecetario.addEventListener("click", function () { changeScreen
 const botonAju = bAjustes.addEventListener("click", function () { changeScreen(5); });
 const clientela = setInterval(summonClient,5000);
 
-//start
+//Start
 changeScreen(2);
 
-
+//Clientela
 function summonClient(){
     if(!hasClient){
+        console.log("Trying to summon Client...");
         if (Math.random() > 0.5) {
-            var client = document.createElement("Cliente");
-            client.classList.add("client");
-            client.setAttribute("id","currentClient");
-            client.setAttribute("onclick","deleteClient()");
-            if(title.innerHTML!="Mostrador"){
-                client.classList.add("hidden");
-            }
+            console.log("Summon Success!");
+            var client = createClient(); 
             screen.appendChild(client);
             hasClient=true;
             texttitle.innerHTML="Hay alguien en tu tienda";
@@ -43,19 +50,74 @@ function summonClient(){
             setTimeout(function(){
                 texttitle.classList.remove("alert");
             }, 500);
+        }else{
+            console.log("Summon failed.");
         }
     }else{
         console.log("There is a Client already.");
     }
 }
+function createClient(){
+    //crear elemento
+    var client = document.createElement("Client");
 
+    //classlist
+    client.classList.add("client");
+    if(title.innerHTML!="Mostrador"){
+        client.classList.add("hidden");
+    }
+    //attributes
+    client.setAttribute("id","currentClient");
+    client.setAttribute("onclick","showClient()");
+
+    var randomint=lastClient;
+    while(randomint==lastClient){
+        randomint = Math.floor(Math.random()*10);
+    }
+    client.setAttribute("name",townsfolk[randomint].name);
+    client.setAttribute("greeting",townsfolk[randomint].greeting);
+            
+    return client;
+}
+function showClient(){
+    var client = document.getElementById("currentClient");
+    texttitle.innerHTML=client.getAttribute("name");
+    box.firstChild.innerHTML=client.getAttribute("greeting");
+    clickToContinue("Delete Client");
+}
 function deleteClient(){
     var client = document.getElementById("currentClient");
     client.remove();
     hasClient=false;
     texttitle.innerHTML="No hay nadie en tu tienda";
+    box.firstChild.innerHTML="...";
+    box.removeEventListener("click",clickToDeleteClient);
 }
 
+//Click TO
+function clickToContinue(action){
+    box.lastChild.classList.remove("hidden");
+    box.lastChild.classList.add("pulsing");
+    switch(action){
+        case "Delete Client": //Comes from "Show Client"
+            clickevent = box.addEventListener("click",clickToDeleteClient);
+            break;
+        default: 
+            console.log("Click to Default");
+            box.lastChild.classList.remove("hidden");
+            box.lastChild.classList.add("pulsing");
+            break;
+    }
+
+}
+function clickToDeleteClient(){
+    deleteClient();
+    box.lastChild.classList.remove("pulsing");
+    box.lastChild.classList.add("hidden");
+}
+
+
+//CHANGE SCREEN
 function changeScreen(option) {
     switch (option) {
         case 1:
@@ -93,7 +155,11 @@ function changeScreen(option) {
             break;
     }
 }
-
+function clearBackground() {
+    screen.classList.remove("jardin");
+    screen.classList.remove("mostrador");
+    screen.classList.remove("trastienda");
+}
 function toggleTextbox(option) {
     switch (option) {
         case "ON":
@@ -105,7 +171,6 @@ function toggleTextbox(option) {
             screen.classList.add("long");
     }
 }
-
 function toggleClient(option) {
     if(hasClient){
         var client = document.getElementById("currentClient");
@@ -120,8 +185,3 @@ function toggleClient(option) {
 }
 }
 
-function clearBackground() {
-    screen.classList.remove("jardin");
-    screen.classList.remove("mostrador");
-    screen.classList.remove("trastienda");
-}
